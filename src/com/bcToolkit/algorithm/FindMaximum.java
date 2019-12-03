dfa68210569f3e4f688c2e04bd863976b13d97c4
bc <brisoncreak@163.com>
2019-12-03 18:09:35 +0800

    maximum

--- src/com/bcToolkit/algorithm/FindMaximum.java
+++ src/com/bcToolkit/algorithm/FindMaximum.java
@@ -1,8 +1,22 @@
 package com.bcToolkit.algorithm;
 
-public class FindMaximum {
-    public static int findMaximumDC(int[] nums) {
-        return 0;
-    }
+import java.lang.Math;
 
+
+public class FindMaximum {
+    public static int findMaximum(int[] nums, int p, int r) {
+        int left = 0, right = 0;
+        int q = (r+p)/2;
+
+        if(p==r) return nums[p];
+
+        if(p==r-1) return Math.max(nums[p], nums[r]);
+
+        if(p < r) {
+            left = findMaximum(nums, p, q);
+            right = findMaximum(nums, q+1, r);
+        }
+
+        return Math.max(left,right);
+    }
 }
--- src/com/bcToolkit/test/Main.java
+++ src/com/bcToolkit/test/Main.java
@@ -5,7 +5,7 @@ import com.bcToolkit.algorithm.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9,10};
-        System.out.print(FindMaximum.findMaximumDC(a));
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(FindMaximum.findMaximum(a, 9, 9));
     }
 }
