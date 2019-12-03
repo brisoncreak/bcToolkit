444008192d989bc11a748182f99341874ba0e412
bc <brisoncreak@163.com>
2019-12-03 14:52:27 +0800

    init

---
+++ src/com/bcToolkit/algorithm/FindMaximum.java
@@ -0,0 +1,8 @@
+package com.bcToolkit.algorithm;
+
+public class FindMaximum {
+    public static int findMaximumDC(int[] nums) {
+        return 0;
+    }
+
+}
--- src/com/bctoolkit/algorithm/sort/MergeSort.java
+++
@@ -1,7 +0,0 @@
-package com.bctoolkit.algorithm.sort;
-
-public class MergeSort {
-    public static void sort(int[] array){
-
-    }
-}
