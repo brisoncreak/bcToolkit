85a93d39a33a50c5b87653bc00239d3296d4daba
bc <brisoncreak@163.com>
2019-12-04 00:14:09 +0800

    fix

--- src/com/bcToolkit/algorithm/QuickSort.java
+++ src/com/bcToolkit/algorithm/QuickSort.java
@@ -9,15 +9,20 @@ public class QuickSort {
             int q = partition(A, p ,r);//计算中间下标
 
             quickSort(A, p, q-1);
-            quickSort(A, q + 1, r);//分而治之 无需合并
+            quickSort(A, q+1, r);//分而治之 无需合并
         }
     }
     public static int partition(int[] A, int p, int r){
-        //int pivot = A[r]; //选最后一个元素作为pivot
-        int pivot = A[RandomGetter.getRandomNum(p,r)];//使用随机能极大提高速度
+        //取得随机元素并与最后一个元素交换位置
+        //int a = RandomGetter.getRandomNum(p,r);
+        //int t = A[r];
+        //A[r] = A[a];
+        //A[a] = t;
+
+        int pivot = A[r]; //选最后一个元素作为pivot
         int i = p-1;//记录结果
         //如果元素小于pivot 则该元素和i处元素互换位置 i++
-        for(int j=p;j<r-1;j++){
+        for(int j=p;j<r;j++){
             if(A[j]<= pivot)
             {
                 i = i+1;
@@ -37,3 +42,22 @@ public class QuickSort {
 
 
 }
+//比较MergeSort和QuickSort
+//        //设定随机生成数组的规模
+//        int length = 1000;
+//        //随机生成整数数组
+//        int[] x = RandomGetter.getRandomArray(length, 0,10000);
+//        int[] y = x.clone();
+//        long startTime1 = System.nanoTime(); //获取开始时间
+//        MergeSort.mergeSort(x,0,length-1);
+//        long endTime1 = System.nanoTime(); //获取结束时间
+//        System.out.println("MergeSort运行时间：" + (endTime1 - startTime1) + "ns"); //输出程序运行时间
+//
+//        for(int i=0;i<100;i++) System.out.print(x[i]+" ");
+//        System.out.print("\n");
+//
+//        long startTime2 = System.nanoTime(); //获取开始时间
+//        QuickSort.quickSort(y,0,length-1);
+//        long endTime2 = System.nanoTime(); //获取结束时间
+//        System.out.println("QuickSort运行时间：" + (endTime2 - startTime2) + "ns"); //输出程序运行时间
+//        for(int i=0;i<100;i++) System.out.print(y[i]+" ");
--- src/com/bcToolkit/test/Main.java
+++ src/com/bcToolkit/test/Main.java
@@ -12,24 +12,10 @@ public class Main {
         int[] b = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
         int[] c= {0,1,2,3,4,5};
         int[] d= {2,8,7,1,3,5,6,4};
+
         //MaxHeapBuilder.buildMaxHeap(b);
 
-        //MergeSort.mergeSort(b,0,9);
 
-        //设定随机生成数组的规模
-        int length = 1000;
-        //随机生成整数数组
-        int[] x = RandomGetter.getRandomArray(length, 0,10000);
-
-        long startTime1 = System.nanoTime(); //获取开始时间
-        MergeSort.mergeSort(x,0,length-1);
-        long endTime1 = System.nanoTime(); //获取结束时间
-        System.out.println("MergeSort运行时间：" + (endTime1 - startTime1) + "ns"); //输出程序运行时间
-
-        long startTime2 = System.nanoTime(); //获取开始时间
-        QuickSort.quickSort(x,0,length-1);
-        long endTime2 = System.nanoTime(); //获取结束时间
-        System.out.println("QuickSort运行时间：" + (endTime2 - startTime2) + "ns"); //输出程序运行时间
 
     }
 }
