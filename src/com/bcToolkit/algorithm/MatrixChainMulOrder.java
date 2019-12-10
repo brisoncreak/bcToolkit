9a93a1ec4235a4731bbc7aec962d196c9dc2819e
bc <brisoncreak@163.com>
2019-12-11 02:09:39 +0800

    clr

--- src/com/bcToolkit/algorithm/MatrixChainMulOrder.java
+++ src/com/bcToolkit/algorithm/MatrixChainMulOrder.java
@@ -42,8 +42,7 @@ public class MatrixChainMulOrder {
         }
     }
 
-
-    //贪心法
+    //贪心法（结果不正确）
     public static int getOrderGreedy(int[] p){
 
         int res = 0;
@@ -88,4 +87,24 @@ public class MatrixChainMulOrder {
             }
             System.out.println();
         }
+
+========比较贪心法与动态规划============
+        //int[] p = {30, 35, 15, 5, 10, 20, 25};
+        //int[] p = {10, 20, 50, 1, 100};
+        //int[] p = {1,2,3,400,5,6};
+
+
+        int[] p = RandomGetter.getRandomArray(5, 1, 100);
+
+        int n=p.length-1;
+        int[][] m = new int[n][n];
+        int[][] s = new int[n][n];
+        int res = MatrixChainMulOrder.getOrder(p, m, s);
+        System.out.println("动态规划最少乘法次数："+res);
+
+        //MatrixChainMulOrder.printOrder(s, 0,n-1);
+        int res0 = MatrixChainMulOrder.getOrderGreedy(p);
+        System.out.println("贪心法最少乘法次数："+res0);
+
+
 */
--- src/com/bcToolkit/test/Main.java
+++ src/com/bcToolkit/test/Main.java
@@ -8,22 +8,6 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        //int[] p = {30, 35, 15, 5, 10, 20, 25};
-        //int[] p = {10, 20, 50, 1, 100};
-        //int[] p = {1,2,3,400,5,6};
-
-
-        int[] p = RandomGetter.getRandomArray(5, 1, 100);
-
-        int n=p.length-1;
-        int[][] m = new int[n][n];
-        int[][] s = new int[n][n];
-        int res = MatrixChainMulOrder.getOrder(p, m, s);
-        System.out.println("动态规划最少乘法次数："+res);
-
-        //MatrixChainMulOrder.printOrder(s, 0,n-1);
-        int res0 = MatrixChainMulOrder.getOrderGreedy(p);
-        System.out.println("贪心法最少乘法次数："+res0);
 
 
 
